f5604d6732599e5b85a1af3cf7e5a85c4163d73a
15900121620@163.com <15900121620@163.com>
2019-06-18 07:56:43 +0800

    feat: 增加脚本参数

--- tampermonkey-demo/hello-world.user.js
+++ tampermonkey-demo/hello-world.user.js
@@ -1,4 +1,20 @@
 /**
  *Created by 夜雪暮歌 on 2019/6/18
  **/
+
+// ==UserScript==
+// @name         Github Info
+// @version      0.1.1
+// @description  A demo to use tampermonkey
+// @author       yxmg
+// @include      *github.com*
+// @exclude      *api.github*
+// @require      https://cdn.bootcss.com/vue/2.5.16/vue.min.js
+// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @run-at       document-body
+// @noframes
+// ==/UserScript==
 console.log('hello world' )
