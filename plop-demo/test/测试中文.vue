724f6bf078f39c1d6b505dfccd996678a62d31a5
15900121620@163.com <15900121620@163.com>
2019-06-25 08:40:22 +0800

    feat: 新增油猴插件：线上打开vue-devtool调试

---
+++ plop-demo/test/测试中文.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    props: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="stylus">
+</style>
