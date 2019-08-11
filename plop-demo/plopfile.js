51743090722d7e47e39c3106e3e6dfb0be4874b7
15900121620@163.com <15900121620@163.com>
2019-08-11 13:10:13 +0800

    feat: 时间处理库API测试 - 获取一年中的第N周 ISOWeek 和 week 的区别

--- plop-demo/plopfile.js
+++ plop-demo/plopfile.js
@@ -6,3 +6,30 @@ const createComponent = require('./plop-templates/component/prompt')
 module.exports = function (plop) {
   plop.setGenerator('component', createComponent)
 }
+
+  [
+  {
+    name: '1-1',
+    level: 1,
+    children: [
+      {
+        name: '2-1',
+        level: 2,
+        children: [
+          { name: '3-1', level: 3, children: null },
+          { name: '3-2', level: 3, children: null },
+          { name: '3-3', level: 3, children: null }
+        ]
+      },
+      {
+        name: '2-2',
+        level: 2,
+        children: [
+          { name: '3-1', level: 3, children: null },
+          { name: '3-2', level: 3, children: null },
+          { name: '3-3', level: 3, children: null }
+        ]
+      }
+    ]
+  }
+  ]
--- time-handler-demo/index.js
+++ time-handler-demo/index.js
@@ -1,3 +1,68 @@
 /**
  *Created by 夜雪暮歌 on 2019/8/11
  **/
+const printResult = (type, moment, dateFns, dayJs) => {
+
+  console.log(`%c/*****${type}*****/`, 'color: #fff; background: #f40; font-size: 18px;border-radius:0 15px 15px 0;padding:5px;')
+  console.log("moment", moment)
+  console.log("dateFns", dateFns)
+  console.log("dayJs", dayJs)
+}
+
+// 打印对象
+printResult(
+  '对象',
+  moment,
+  dayjs,
+  dateFns
+)
+
+// 字符串 + 日期格式化
+printResult(
+  '字符串 + 日期格式化',
+  moment('2019年8月11日', 'YYYY年MM月DD日'), // 支持
+  dateFns.parse('2019年8月11日', 'yyyy年MM月dd日', new Date()), // 不支持中文格式
+  dayjs('2019年8月11日') // 不支持自定义格式
+)
+
+// 获取时分秒
+printResult(
+  '获取时分秒',
+  `时：${moment().hours()}，分：${moment().minutes()}，秒：${moment().seconds()}`,
+  `时：${dateFns.getHours(new Date())}，分：${dateFns.getMinutes(new Date())}，秒：${dateFns.getSeconds(new Date())}`,
+  `时：${dayjs().hour()}，分：${dayjs().minute()}，秒：${dayjs().second()}`,  // dayJs没有hours等方法，moment有，所以并不是完全兼容
+)
+
+// 获取月份
+printResult(
+  '获取月份',
+  moment().date() + '月',
+  dateFns.getDate(new Date()) + '月',
+  dayjs().date() + '月'
+)
+
+// 获取星期几
+printResult(
+  '获取星期几',
+  '星期' + moment().day(),
+  '星期' + dateFns.getDay(new Date()),
+  '星期' + dayjs().day(),
+)
+
+// 获取一年中的第N天
+printResult(
+  '获取一年中的第N天',
+  moment().dayOfYear(),
+  dateFns.getDayOfYear(new Date()),
+  // dayjs().dayOfYear(), // dayJs不支持
+)
+
+// 获取一年中的第N周
+printResult(
+  '获取一年中的第N周',
+  moment().week(),
+  dateFns.getISOWeek(new Date()), // ISO 对周的国际标准,表现为比moment少一周
+  // dayjs.week() // dayJs不支持
+)
+
+
