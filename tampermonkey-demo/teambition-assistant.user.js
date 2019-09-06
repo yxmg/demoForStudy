b89fd8fb9b2af104ab1921c402d34efdcf091034
guanmingzhao <guanmz@chaosource.com>
2019-09-06 09:49:33 +0800

    feat: TB快速申请脚本-创建

---
+++ tampermonkey-demo/teambition-assistant.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         TB快速申请插件
+// @version      0.1.1
+// @description  尚未完善，风险自担
+// @author       yxmg
+// @match        *://www.teambition.com/*
+// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@8
+// @require      https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @require      https://cdn.bootcss.com/jquery-contextmenu/2.8.0/jquery.contextMenu.min.js
+// @require      https://cdn.bootcss.com/jquery-contextmenu/2.8.0/jquery.ui.position.min.js
+// @require      https://cdn.bootcss.com/localforage/1.7.3/localforage.min.js
+// @resource     contextMenuCss https://cdn.bootcss.com/jquery-contextmenu/2.8.0/jquery.contextMenu.min.css
+// @resource     fontAwesomeIconCss https://cdn.bootcss.com/font-awesome/5.10.0-11/css/fontawesome.min.css
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant       GM_getResourceText
+// @run-at       document-body
+// @noframes
+// ==/UserScript==
+(function () {
+    console.log('test');
+})()
