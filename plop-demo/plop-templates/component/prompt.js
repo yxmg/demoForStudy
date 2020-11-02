33514365879dd4e8c9e845db78ea59e2a7076f3f
管铭钊 <guanmz@chaosource.com>
2020-11-02 08:31:26 +0800

    chore: plop自动化Demo

--- plop-demo/plop-templates/component/prompt.js
+++ plop-demo/plop-templates/component/prompt.js
@@ -1,29 +1,70 @@
 /**
  *Created by 夜雪暮歌 on 2019/6/13
  **/
+
 module.exports = {
-  description: '创建vue文件',
-  prompts: [
-    {
-      type: 'input',
-      name: 'fileName',
-      message: '请输入文件名：'
-    },
-    {
-      type: 'list',
-      name: 'styleType',
-      message: '请选择一种CSS预处理器：',
-      choices: [
-        'scss',
-        'stylus'
-      ]
-    }
-  ],
-  actions: [
-    {
-      type: 'add',
-      path: 'test/{{fileName}}.vue',
-      templateFile: './plop-templates/component/index.hbs'
-    }
-  ]
+    description: '创建Vue组件',
+    prompts: [
+        {
+            type: 'input',
+            name: 'fileName',
+            message: '请输入文件名：'
+        },
+        {
+            type: 'checkbox',
+            name: 'blocks',
+            message: '请取消勾选不需要的模块（默认全选）',
+            choices: [
+                {
+                    name: '<template>',
+                    value: 'template',
+                    checked: true
+                },
+                {
+                    name: '<script>',
+                    value: 'script',
+                    checked: true
+                },
+                {
+                    name: 'style',
+                    value: 'style',
+                    checked: true
+                }
+            ],
+            validate(value) {
+                if (
+                    value.indexOf('script') === -1 &&
+                    value.indexOf('template') === -1
+                ) {
+                    return 'Components require at least a <script> or <template> tag.';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'isGlobal',
+            message: '请选择组件作用域',
+            choices: [
+                {
+                    name: 'global',
+                    value: '全局',
+                    checked: true
+                },
+                {
+                    name: 'local',
+                    value: '局部',
+                    checked: true
+                },
+            ],
+            default: false
+        }
+    ],
+    actions: [
+        {
+            type: 'add',
+            path: 'test/{{fileName}}.vue',
+            templateFile: './plop-templates/component/index.hbs',
+        }
+    ]
 }
